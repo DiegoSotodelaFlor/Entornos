db62b29f5b9c7e15e4fe85bef54e114054b27676
DiegoSotodelaFlor <diegosotodelaflor@gmail.com>
2025-11-25 14:52:00 +0100

    1

--- ProyectoSistemadeGestiondePedidos/src/SistemadeGestiondePedidos/Pedido.java
+++ ProyectoSistemadeGestiondePedidos/src/SistemadeGestiondePedidos/Pedido.java
@@ -2,16 +2,24 @@ import java.util.ArrayList;
 
 public class Pedido {
     //Atributos
+    private int numeroDePedido;
     private ArrayList<Producto> productos;
     private Cliente c;
     //Constructor
     public Pedido(Cliente c){
         this.c=c;
         this.productos = new ArrayList<Producto>();
+        this.numeroDePedido=numeroDePedido;
     }
-    //Metodo get
+    //Metodos get y set
     public ArrayList<Producto> getProductos(){
         return productos;
+    }
+    public int getNumero(){
+        return numeroDePedido;
+    }
+    public void setNumero(int numeroDePedido){
+        this.numeroDePedido=numeroDePedido;
     }
     //Metodos y funciones
     public void agregarProducto(Producto p){
@@ -25,13 +33,13 @@ public class Pedido {
         return total;
     }
     public String mostrarResumen(Cliente c){
-    String resumen="**************************\nPedido del cliente: \n"+c.mostrarInfo()+"\n**************************\nProductos: \n";
+    String resumen="--------------------------\nPedido nº: "+numeroDePedido+"\n**************************\nCliente: \n"+c.mostrarInfo()+"\n**************************\nProductos: \n";
     //Recorremos el array de productos para mostrar cada producto
     for (Producto p:productos){
         resumen+=p.mostrarInfo()+"\n";
     }
     resumen+="**************************\nTotal: "+String.format("%.2f euros",calcularTotal());
-    return resumen+"\n**************************";
+    return resumen+"\n--------------------------";
     }
     @Override
     public String toString() {
